28cff9906e1c96e0d4d602c38ddc9f4ce154bc69
Mahmood Nalim <mahmoodnalim@gmail.com>
2018-12-23 11:57:04 +0530

    New Change

--- models/rides.js
+++ models/rides.js
@@ -2,28 +2,52 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const ridesSchema = new Schema({
-  driver: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
-  riders: { type: [mongoose.Schema.Types.ObjectId], ref: "user" },
-  origins: {
+  driver: {
+    type: {
+      did: mongoose.Schema.Types.ObjectId,
+      name: String,
+      contact: String
+    }
+  },
+  riders: {
     type: [
       {
-        lat: Number,
-        lng: Number,
-        uid: { type: mongoose.Schema.Types.ObjectId }
+        origin: { lat: Number, lng: Number },
+        destination: {
+          lat: Number,
+          lng: Number
+        },
+        userDetails: {
+          type: {
+            name: String,
+            contact: String
+          }
+        },
+        uid: { type: mongoose.Schema.Types.ObjectId, ref: "user" }
       }
-    ],
-    ref: "user"
+    ]
   },
-  destinations: {
-    type: [
-      {
-        lat: Number,
-        lng: Number,
-        uid: { type: mongoose.Schema.Types.ObjectId }
-      }
-    ],
-    ref: "user"
-  },
+
+  // origins: {
+  //   type: [
+  //     {
+  //       lat: Number,
+  //       lng: Number,
+  //       uid: { type: mongoose.Schema.Types.ObjectId }
+  //     }
+  //   ],
+  //   ref: "user"
+  // },
+  // destinations: {
+  //   type: [
+  //     {
+  //       lat: Number,
+  //       lng: Number,
+  //       uid: { type: mongoose.Schema.Types.ObjectId }
+  //     }
+  //   ],
+  //   ref: "user"
+  // },
   price: { type: Number, required: true }
 });
 
